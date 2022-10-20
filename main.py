ee7b6fde0c70b970bd2a1fecded8821010ac1111
Arturo0911 <anegreiross@outlook.com>
2022-10-20 01:21:19 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from torch import nn
+
+
+class Net(nn.Module):
+    def __init__(self):
+        pass
+
+
+def main():
+    try:
+        print("dick")
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    main()
