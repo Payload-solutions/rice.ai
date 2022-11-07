fbc2aff0398d0b2052e3652553df585da840a66b
Arturo0911 <anegreiross@outlook.com>
2022-11-07 01:55:26 -0500

    chart and doughnut created ok

--- cnn-client/src/app/components/line/line.component.ts
+++ cnn-client/src/app/components/line/line.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Chart, registerables } from 'chart.js';
+
 @Component({
   selector: 'app-line',
   templateUrl: './line.component.html',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineComponent implements OnInit {
 
-  constructor() { }
+  public chart: any;
+
+  constructor() {
+    Chart.register(...registerables);
+  }
 
   ngOnInit(): void {
+    this.generateChart();
+  }
+
+  generateChart() {
+    this.chart = new Chart("MyChart1", {
+      type: 'doughnut', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        aspectRatio: 2.5
+      }
+
+    });
   }
 
 }
