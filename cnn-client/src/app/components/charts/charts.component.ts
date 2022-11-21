f6cf91c2c6fe780a53674ce344181c3282ea43cf
Arturo0911 <anegreiross@outlook.com>
2022-11-20 20:55:37 -0500

    frontend 75% finished

--- cnn-client/src/app/components/charts/charts.component.ts
+++ cnn-client/src/app/components/charts/charts.component.ts
@@ -21,31 +21,47 @@ export class ChartsComponent implements OnInit {
   }
 
   createChart(){
+    const labels: string[] | any = [];
+
+    for (var a = [], i = 0; i < 100; ++i) a[i] = i;
+    for (var b = [], i = 0; i < 100; ++i) b[i] = i;
+
+    // http://stackoverflow.com/questions/962802#962890
+    function shuffle(array: number[]) {
+      var tmp, current, top = array.length;
+      if (top) while (--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+      }
+      return array;
+    }
+
+    a = shuffle(a);
+    b = shuffle(b);
+
+
+    for (let i = 0; i <= 100; i++) {
+      labels.push(i.toString());
+    }
     this.chart = new Chart("MyChart", {
-      type: 'bar', //this denotes tha type of chart
+      type: 'line', //this denotes tha type of chart
 
       data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
-	       datasets: [
-          {
-            label: "Sales",
-            data: ['467','576', '572', '79', '92',
-								 '574', '573', '576'],
-            backgroundColor: 'blue'
-          },
-          {
-            label: "Profit",
-            data: ['542', '542', '536', '327', '17',
-									 '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }  
-        ]
+        labels: labels,
+        datasets: [{
+          label: 'Registro de macronutrientes del suelo',
+          data: a,//[65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
       },
       options: {
-        aspectRatio:2.5
+        aspectRatio: 2.5
       }
-      
+
     });
   }
 
--- cnn-client/src/app/services/authentication.service.ts
+++ cnn-client/src/app/services/authentication.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
 
   isLoggedIn(): boolean{
     return !!localStorage.getItem("token");
+  }
+
+  isConvolutionSessionIn():boolean{
+    return !!localStorage.getItem("conv-session");
+  }
+
+  logOut(): void{
+    this.router.navigate(['/login']);
   }
 }
