26b289ccc4a4409f4a65a6defc7c1dace6f386c6
Arturo0911 <anegreiross@outlook.com>
2022-11-23 01:55:58 -0500

    ok

--- cnn-server/apps/users/models.py
+++ cnn-server/apps/users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin
+)
+from simple_history import HistoricalRecords
 
 # Create your models here.
+
+
+
+class UserManager():
+    pass
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField()
+    email = models.CharField()
+    name = models.CharField()
+    last_name = models.CharField()
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    historical = HistoricalRecords()
