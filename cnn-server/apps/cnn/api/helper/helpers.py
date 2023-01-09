06df01261c680367c41cae71316f5489c6772fd6
Arturo0911 <anegreiross@outlook.com>
2023-01-09 16:44:01 -0500

    ok

--- cnn-server/apps/cnn/api/helper/helpers.py
+++ cnn-server/apps/cnn/api/helper/helpers.py
@@ -18,8 +18,7 @@ def recommendation(conv_value_nitrogen, conv_value_loss):
     result = abs(float(conv_value_nitrogen) - float(conv_value_loss))
     # if float(conv_value_nitrogen) <= 60 and float(conv_value_loss) <= 60:
     if result <= 2.0:
-        return "La imagen dada no corresponde a una imagen de planta de arroz, es probable que no haya coincidencia " \
-               "de pixeles o que los angulos no permitan la detección de la misma, prueba otra imagen"
+        return "La imagen no corresponde a una imagen de arroz o quizás los pixeles no permiten su aceptación, prueba otra imagen"
 
     return f"La planta se presenta un rango de concentración de nitrtógeno de  {float(conv_value_nitrogen):.2f} %" if float(
-        conv_value_nitrogen) >= 85.00 else f"Coloración amarilla con una deficiencia de {float(abs(97 - float(conv_value_loss))):.2f}; considerar incluir mayor cantidad de (NO3-N) en el cultivo"
+        conv_value_nitrogen) >= 85.00 else f"Coloración amarilla con una deficiencia de {float(abs(97 - float(conv_value_loss))):.2f} %; considerar incluir mayor cantidad de (NO3-N) en el cultivo"
