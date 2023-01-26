b130e584c61f7257e21cfc026243dfeda0d7243c
Arturo0911 <anegreiross@outlook.com>
2023-01-26 04:09:20 -0500

    updating before presentation

--- cnn-server/apps/cnn/api/helper/helpers.py
+++ cnn-server/apps/cnn/api/helper/helpers.py
@@ -17,8 +17,12 @@ def prediction_difference(conv_value_nitrogen, conv_value_loss):
 def recommendation(conv_value_nitrogen, conv_value_loss):
     result = abs(float(conv_value_nitrogen) - float(conv_value_loss))
     # if float(conv_value_nitrogen) <= 60 and float(conv_value_loss) <= 60:
-    if result <= 2.0:
+    print(f"result nitrogen {float(conv_value_nitrogen)} and loss: {float(conv_value_loss)}")
+    if result <= 0.5:
         return "No es una imagen de arroz, prueba con otra"
+
+    if result >= 89:
+        return "No es una planta de arroz, prueba con otra"
 
     return f"presencia de nitrógeno en la imagen  {float(conv_value_nitrogen):.2f} %" if float(
         conv_value_nitrogen) >= 85.00 else f"Deficiencia de Nitrógeno de {float(abs(97 - float(conv_value_loss))):.2f} %; considerar incluir mayor cantidad de (NO3-N) en el cultivo"
