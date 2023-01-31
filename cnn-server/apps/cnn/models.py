29fca7ca1f818a7e81cd37a336ad047af5cf7baf
Arturo0911 <anegreiross@outlook.com>
2023-01-31 13:32:48 -0500

    updating model

--- cnn-server/apps/cnn/models.py
+++ cnn-server/apps/cnn/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from apps.base.models import BaseModel
 from simple_history.models import HistoricalRecords
 
-
 import tensorflow as tf
 from tensorflow.python.keras.models import (
     load_model,
@@ -33,7 +32,7 @@ class Classification(BaseModel):
         """Meta definition for the classification cnn"""
         verbose_name = "Classification"
         verbose_name_plural = "Classifications"
-    
+
     def predict(self):
         K.reset_uids()
         model = "apps/cnn/models/riceai_json_model_2.json"
@@ -43,18 +42,16 @@ class Classification(BaseModel):
             with open(model, 'r') as f:
                 model = model_from_json(f.read())
                 model.load_weights(weights)
-        
+
         image = Image.open(self.img)
-        img = img_to_array(load_img(self.img, target_size=(224,224)))/255.0
+        img = img_to_array(load_img(self.img, target_size=(224, 224))) / 255.0
         img = img.reshape(1, 224, 224, 3)
 
         result = model.predict(img)
-        
+
         return result[0][0], result[0][1]
-    
 
     def save(self, *args, **kwargs):
-
         K.reset_uids()
         model = "apps/cnn/models/riceai_json_model_2.json"
         weights = "apps/cnn/models/final_model_2.hdf5"
@@ -63,20 +60,16 @@ class Classification(BaseModel):
             with open(model, 'r') as f:
                 model = model_from_json(f.read())
                 model.load_weights(weights)
-        
-        image = Image.open(self.img)
-        image = image.resize((224,224))
-        x = img_to_array(image)/255.0
-        x = x.reshape(1, 224, 224, 3)
 
+        image = Image.open(self.img)
+        image = image.resize((224, 224))
+        x = img_to_array(image) / 255.0
+        x = x.reshape(1, 224, 224, 3)
 
         result = model.predict(x)
 
-        self.accuracy_healthy, self.loss_nitrogen = str(float(result[0][0])*100), str(float(result[0][1])*100)
+        self.accuracy_healthy, self.loss_nitrogen = str(float(result[0][0]) * 100), str(float(result[0][1]) * 100)
         super().save(*args, **kwargs)
-        
 
     def __str__(self):
         return f"Accuracy healthy leaf: {self.accuracy_healthy} Accuracy Loss nitrogen: {self.loss_nitrogen}"
-
-
